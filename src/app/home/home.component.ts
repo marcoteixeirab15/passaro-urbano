c016a537356178513e7d4b3e562c9eaddf5096ab
marco.barbosa <marco.barbosa@basis.com.br>
2020-10-19 16:06:32 -0300

    Injeta ofertaService e cria OfertaModel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[];
+
+  constructor(private ofertaService: OfertasService) { }
 
   ngOnInit(): void {
+    this.ofertas = this.ofertaService.getOfertas();
+    console.log(this.ofertas);
   }
 
 }
